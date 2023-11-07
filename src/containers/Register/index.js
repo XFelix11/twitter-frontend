2595266804e6996fd03f2c9152a5bb66b5e99435
Felix <xiefx11@163.com>
2023-11-06 22:36:46 -0500

    feat: 🎸 completed components using react 5-step method

--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { Button, Form } from 'antd-mobile';
+import { Form } from 'antd-mobile';
 import Header from '@components/Header';
 import DatePickerInput from '@components/DatePickerInput';
 import TInput from '@components/TInput';
+import Footer from './components/Footer';
 
 import style from './index.module.scss';
 
@@ -23,6 +24,7 @@ const Register = () => {
   });
 
   const [accountType, setAccountType] = useState(ACCOUNT_TYPE.TEL);
+  const [footerButtonDisabled, setFooterButtonDisabled] = useState(true);
 
   const onAccountChange = () => {
     if (accountType === ACCOUNT_TYPE.TEL) {
@@ -39,12 +41,32 @@ const Register = () => {
     }
   };
 
+  const onValuesChange = async () => {
+    try {
+      const validate = await form.validateFields();
+      if (validate) {
+        setFooterButtonDisabled(false);
+      }
+    } catch (e) {
+      if (e.errorFields.length === 0) {
+        setFooterButtonDisabled(false);
+        return;
+      }
+      setFooterButtonDisabled(true);
+    }
+  };
+
   return (
     <div>
       <Header />
       <div className={style.form}>
         <div className={style.formTitle}>Create Your Account</div>
-        <Form form={form} initialValues={formData} className={style.formContainer}>
+        <Form
+          form={form}
+          initialValues={formData}
+          onValuesChange={onValuesChange}
+          className={style.formContainer}
+        >
           <Form.Item
             name="name"
             rules={[{ required: true, message: 'Please input your name.' }]}
@@ -55,23 +77,23 @@ const Register = () => {
             {accountType === ACCOUNT_TYPE.TEL && (
             <Form.Item
               name="tel"
-              rules={[{ required: true, message: 'Please input your phone number.' }]}
+              rules={[{ required: true, message: 'Please input a valid phone number.', pattern: /\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}/g }]}
             >
-              <TInput length={11} label="Phone Number" />
+              <TInput length={10} label="Phone Number" />
             </Form.Item>
             )}
             {accountType === ACCOUNT_TYPE.EMAIL && (
             <Form.Item
               name="email"
-              rules={[{ required: true, message: 'Please input your email.' }]}
+              rules={[{ required: true, message: 'Please input a valid email.', pattern: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g }]}
             >
               <TInput length={254} label="email" />
             </Form.Item>
             )}
           </Form.Item>
-          <div className={style.typeButton} onClick={onAccountChange}>
+          <span className={style.typeButton} onClick={onAccountChange}>
             {accountType === ACCOUNT_TYPE.EMAIL ? 'Use Phone Number' : 'Use Email'}
-          </div>
+          </span>
           <div className={style.birthday}>Date of Birth</div>
           <div>This will not be shown publicly. Confirm your own age.</div>
           <Form.Item name="birthday">
@@ -79,9 +101,7 @@ const Register = () => {
           </Form.Item>
         </Form>
       </div>
-      <div className={style.footer}>
-        <Button className={style.footerButton} onClick={onClickNextStep}>Next</Button>
-      </div>
+      <Footer disabled={footerButtonDisabled} onClickNextStep={onClickNextStep} />
     </div>
   );
 };
